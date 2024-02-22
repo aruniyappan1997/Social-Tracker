0fb068d12416ba3039f89e37fc1cebf80c618cc7
aruniyappan1997 <aruniyappan1997@gmail.com>
2024-02-23 00:10:12 +0530

    Login, Logout, Home Page Created without authentication

--- SIIM/SIIM_app/urls.py
+++ SIIM/SIIM_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import Home
+from .views import Home, Login, Logout
 
 urlpatterns = [
-    path('', Home, name="home"),
+    path('', Login, name="login"),
+    path('home/', Home, name="home"),
+    path('logout/', Logout, name="logut"),
 ]
--- SIIM/SIIM_app/views.py
+++ SIIM/SIIM_app/views.py
@@ -3,5 +3,11 @@ from django.template import loader
 
 # Create your views here.
 def Home(request):
-    return render(request,"Login.html")
+    return render(request,"Home.html")
+
+def Login(request):
+    return render(request, "Login.html")
+
+def Logout(request):
+    return render(request, "Logout.html")
     

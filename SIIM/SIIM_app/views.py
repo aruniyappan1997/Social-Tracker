217c1401fe1c89786cde89f70d74d94ae84410b8
aruniyappan1997 <aruniyappan1997@gmail.com>
2024-02-22 23:44:52 +0530

    Login page Created

--- SIIM/SIIM_app/views.py
+++ SIIM/SIIM_app/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
 from django.template import loader
-from django.http import HttpResponse
 
 # Create your views here.
 def Home(request):
-    template = loader.get_template("Login.html")
-    return HttpResponse(template.render())
+    return render(request,"Login.html")
     
